c69945193c1697d4c906d8ba37fc1a0dfa190620
mokgosi <mokgosi@gmail.com>
2025-04-03 23:24:35 +0200

    Refactor functions to proper accept ({event})

--- config/inngest.js
+++ config/inngest.js
@@ -12,16 +12,21 @@ export const syncUserCreate = inngest.createFunction(
     }, {
         event: 'clerk/user.created'
     },
-    async (event) => {
+    async ({event}) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
-        const userData = {
-            _id: id,
-            email: email_addresses[0].email_address,
-            name: `${first_name} ${last_name}`,
-            imageUrl: image_url
-        }
+        
         await dbConnect()
-        await User.create(userData);
+
+        const user = await User.findOne({ id })
+
+        if (!user) {
+            await User.create({
+                _id: id,
+                email: email_addresses[0].email_address,
+                name: `${first_name} ${last_name}`,
+                imageUrl: image_url
+            });
+        }
     }
 )
 
@@ -32,7 +37,7 @@ export const syncyUserUpdate = inngest.createFunction(
     }, {
         event: 'clerk/user.updated'
     },
-    async (event) => {
+    async ({event}) => {
         const { id, first_name, last_name, email_addresses, image_url } = event.data
         
         await dbConnect()
@@ -52,7 +57,7 @@ export const syncyUserDelete = inngest.createFunction(
     }, {
         event: 'clerk/user.deleted'
     },
-    async (event) => {
+    async ({event}) => {
         const { id } = event.data
         
         await dbConnect()
